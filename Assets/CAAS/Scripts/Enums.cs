c2637aef92bfbaa97049f67cb9fd71ab4b602ad2
mayankjunglee <mayank.ghanshala@jungleegames.com>
2020-09-29 13:53:09 +0530

    Cards Sprite and enum

---
+++ Assets/CAAS/Scripts/Enums.cs
@@ -0,0 +1,20 @@
+﻿public enum CardType
+{
+    CLUB,HEART,SPADE,DIAMOND
+}
+public enum CardValye
+{
+    A = 1,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    J,
+    Q,
+    K
+}
