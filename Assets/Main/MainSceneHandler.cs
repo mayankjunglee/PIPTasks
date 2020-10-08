a76f4ad121ff16b0be5e0b5f103b40d54b3e5ce5
mayankjunglee <mayank.ghanshala@jungleegames.com>
2020-10-08 13:36:35 +0530

    All Scenes

---
+++ Assets/Main/MainSceneHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainSceneHandler : MonoBehaviour
+{
+    // public void LoadScene()
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
